af82dfe52a99f01a81807b656db0e3ad3f2460a5
Alexei196 <dndy778@gmail.com>
2023-01-31 16:30:24 -0800

    Update CRUDServer.java

--- src/crudpackage/CRUDServer.java
+++ src/crudpackage/CRUDServer.java
@@ -4,14 +4,14 @@ import java.net.*;
 import java.io.*;
 
 public class CRUDServer {
-
+	int maxConnections = 8;
 	public static void main(String[] args) {
 		try {
 			ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
-			
 			while(true) {
 				Socket clientSocket = serverSocket.accept();
 				ConnectionHandler newClientConnection = new ConnectionHandler(clientSocket);
+				clientSocket.
 				System.out.println("Client Connected");
 			}
 		} catch (NumberFormatException e) {
